3665c7464025fb749864946d3fdc1261cb6ea91f
Anton <abmoRobotics@student.aau.dk>
2022-10-19 09:01:23 +0200

    Ray casting implemented

--- omniisaacgymenvs/robots/articulations/views/rover_view.py
+++ omniisaacgymenvs/robots/articulations/views/rover_view.py
@@ -40,8 +40,11 @@ class RoverView(ArticulationView):
         self._actuated_dof_indices = [i for i in range(self.num_dof)]
         # self._actuated_vel_indices = [9,10,11,12,13,14] exomy
         # self._actuated_pos_indices = [3,4,5,6,7,8] exomy
-        self._actuated_vel_indices = [10, 11, 14, 7, 8, 13] #[FR, CR, RR, FL, CL, RL]
-        self._actuated_pos_indices = [5, 12, 3, 9]#[FR ,RR, FL, RL]
+        # self._actuated_vel_indices = [10, 11, 14, 7, 8, 13] #[FR, CR, RR, FL, CL, RL] # WITH DIFFERENTIAL
+        # self._actuated_pos_indices = [5, 12, 3, 9]#[FR ,RR, FL, RL] # WITH DIFFERENTIAL
+        self._actuated_vel_indices = [10, 5, 12, 9, 3, 11] #[FR, CR, RR, FL, CL, RL] 
+        self._actuated_pos_indices = [6, 8, 4, 7]#[FR ,RR, FL, RL]
+        self._passive_pos_indices = [6, 8, 4, 7]#[FR ,RR, FL, RL]
         print("Initializing123")
         print(self._actuated_dof_indices)
         # self.physics_rotors = [RigidPrimView(prim_paths_expr=f"/World/envs/.*/Ingenuity/rotor_physics_{i}", name=f"physics_rotor_{i}_view", reset_xform_properties=False) for i in range(2)]
