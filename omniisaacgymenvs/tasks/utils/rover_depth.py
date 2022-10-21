735d57621e1ced110f9567f1bc32451c563dd230
Anton Bjorndahl Mortensen <abmo19@student.aau.dk>
2022-10-21 13:43:54 +0200

    Fix for point cloud depth transform

--- omniisaacgymenvs/tasks/utils/rover_depth.py
+++ omniisaacgymenvs/tasks/utils/rover_depth.py
@@ -24,10 +24,10 @@ def depth_transform(rover_l, rover_r, rover_depth_points):
     z = torch.cat((z, torch.tensor([-1], device='cuda:0').expand(1, 1)), 1)
 
     # Compute sin and cos to all angles - Sizes: [n, 1]
-    sinxr = torch.transpose(torch.sin(rover_r[:,0].expand(1, num_robots)), 0, 1)
-    cosxr = torch.transpose(torch.cos(rover_r[:,0].expand(1, num_robots)), 0, 1)
-    sinyr = torch.transpose(torch.sin(rover_r[:,1].expand(1, num_robots)), 0, 1)
-    cosyr = torch.transpose(torch.cos(rover_r[:,1].expand(1, num_robots)), 0, 1)
+    sinxr = torch.transpose(torch.sin(-rover_r[:,0].expand(1, num_robots)), 0, 1)
+    cosxr = torch.transpose(torch.cos(-rover_r[:,0].expand(1, num_robots)), 0, 1)
+    sinyr = torch.transpose(torch.sin(-rover_r[:,1].expand(1, num_robots)), 0, 1)
+    cosyr = torch.transpose(torch.cos(-rover_r[:,1].expand(1, num_robots)), 0, 1)
     sinzr = torch.transpose(torch.sin(-rover_r[:,2].expand(1, num_robots)), 0, 1)
     coszr = torch.transpose(torch.cos(-rover_r[:,2].expand(1, num_robots)), 0, 1)
 
@@ -37,9 +37,9 @@ def depth_transform(rover_l, rover_r, rover_depth_points):
     rover_zl = torch.transpose(rover_l[:, 2].expand(num_points,num_robots), 0, 1)
 
     # Transform points in the pointcloud
-    x_p = rover_xl + cosyr * (x*coszr + y*sinzr) - z*sinyr
-    y_p = rover_yl + cosxr * (y*coszr - x*sinzr) + sinxr * (z*cosyr + sinyr*(x*coszr + y*sinzr))
-    z_p = rover_zl + cosxr * (z*cosyr + sinyr * (x*coszr + y*sinzr)) - sinxr * (y*coszr - x*sinzr)
+    x_p = rover_xl + sinzr * (y*cosxr + z*sinxr) + coszr * (x*cosyr - sinyr*(z*cosxr - y*sinxr))
+    y_p = rover_yl + coszr * (y*cosxr + z*sinxr) - sinzr * (x*cosyr - sinyr*(z*cosxr - y*sinxr))
+    z_p = rover_zl + x*sinyr + cosyr*(z*cosxr - y*sinxr)
     
     # Extract the plane-normal as the last point
     rover_dir = [x_p[:,num_points-1]-rover_l[:, 0], y_p[:,num_points-1]-rover_l[:, 1], z_p[:,num_points-1]-rover_l[:, 2]]
@@ -72,10 +72,18 @@ def draw_depth(heightmap_points: torch.tensor, depth_points: torch.tensor,):
         draw.draw_lines(rover_distribution, depth_points, [(0.9, 0.5, 0.1, 0.9)]*N, [3]*N)
 
 
-def draw_point(points):
+def draw_point(points, color):
     draw = _debug_draw.acquire_debug_draw_interface()
     N = 1
 
     point_list = points.tolist()
     draw.clear_points()
-    draw.draw_points([point_list], [(0.9, 0.5, 0.9, 0.9)], [55])
+    draw.draw_points([point_list], color, [55])
+
+def draw_coord():
+    draw = _debug_draw.acquire_debug_draw_interface()
+    N = 4
+    point_list = [[0,0,0],[1,0,0],[0,1,0],[0,0,1]]
+    colors = [[0.1, 0.1, 0.1, 0.9],[1.0, 0.0, 0.0, 0.9],[0.0, 1.0, 0.0, 0.9],[0.0, 0.0, 1.0, 0.9]]
+    draw.clear_points()
+    draw.draw_points(point_list, colors, [55]*N)
