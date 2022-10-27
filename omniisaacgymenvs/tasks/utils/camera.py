70b568fe020e1d1d8705391bde4dec96f7c21882
abmoRobotics <abmo19@student.aau.dk>
2022-10-27 10:30:52 +0200

    Camera Working

--- omniisaacgymenvs/tasks/utils/camera.py
+++ omniisaacgymenvs/tasks/utils/camera.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-
+import open3d as o3d
 from omniisaacgymenvs.tasks.utils.heightmap_distribution import heightmap_distribution
 from omniisaacgymenvs.tasks.utils.rover_depth import draw_depth
 
@@ -12,69 +12,85 @@ class Camera():
         self.num_partitions = 1
         self.horizontal = 0.05
         self.map_values = self._load_triangles()
+        self.map_indices, self.triangles, self.vertices = self._load_triangles_with_indices()
         #TODO Refeactor this
         self.map_values=self.map_values.swapaxes(0,1)
         self.map_values=self.map_values.swapaxes(1,2)
         self.heightmap_distribution = self._heightmap_distribution()
         self.shift = shift
 
+
     def get_depths_test(self, sources, directions):
         triangles = self._height_lookup(self.map_values,sources[:,:,0:2],self.horizontal,self.shift)
-        triangles = torch.tensor([[[[[0.0,0.0,0.0],[0.0,2.0,0.0],[2.0,0.0,0.0]]]]],device='cuda:0')
-
+        triangles = torch.tensor([[[[[0.0,0.0,0.0],[0.0,2.0,0.0],[2.0,0.0,0.0]],[[0.0,0.0,0.2],[0.0,2.0,0.2],[2.0,0.0,0.2]],[[0.0,0.0,0.3],[0.0,2.0,0.3],[2.0,0.0,0.3]]]]],device='cuda:0')
+        #print("triangles", triangles.shape)
         dim0, dim1, dim2 = sources.shape[0], sources.shape[1], sources.shape[2]
         t_dim0, t_dim1, t_dim2, t_dim3, t_dim4 = triangles.shape[0], triangles.shape[1], triangles.shape[2], triangles.shape[3], triangles.shape[4] 
         s = sources.reshape(dim0*dim1,dim2)
         d = directions.reshape(dim0*dim1,dim2)
         t = triangles.reshape(t_dim0*t_dim1*t_dim2,t_dim3,t_dim4)
+
         distances,pt = self._ray_distance(s,d,t)
         distances = distances.reshape(dim0,dim1,t_dim2)
+        pt = pt.reshape(dim0,dim1,t_dim2,3)
+        # Find the maximum value of the 100 triangles to extract the "heightmap" value.
+        indices =torch.max(distances,2).indices
         distances = torch.max(distances,2).values
+        indices = indices.unsqueeze(dim=2).unsqueeze(dim=2).repeat(1,1,1,3)
+        pt = pt.gather(dim=2,index=indices)
+        pt = pt.squeeze(dim=2)
         return distances,pt
 
     def get_depths(self, positions, rotations):
         # 
+
         sources, directions = self._depth_transform(positions,rotations,self.heightmap_distribution)
-        print(sources.shape)
+
         depth_points = sources[:,:,0:2]
-        # print('Output of height lookup')
-        # print(depth_points.shape)
-        triangles = self._height_lookup(self.map_values,depth_points,self.horizontal,self.shift)
-        # print(triangles.shape)
-        #print(sources.shape)
+        triangles,triangles_with_indices = self._height_lookup(self.map_values,depth_points,self.horizontal,self.shift)
+
+        #self._plot_mesh(triangles_with_indices.flatten())
+
         # We partition the data to reduce VRAM usage
         if self.partition:
             partitions = self.num_partitions
             output_distances = None
             for i in range(partitions):
                 step_size = int(sources.shape[1]/partitions)
-                # # print(sources.shape)
-                # # print(directions.shape)
+         
+          
                 s = sources[:,i*step_size:i*step_size+step_size]
+     
                 d = directions[:,i*step_size:i*step_size+step_size]
                 t = triangles[:,i*step_size:i*step_size+step_size]
+       
                 dim0, dim1, dim2 = s.shape[0], s.shape[1], s.shape[2]
                 t_dim0, t_dim1, t_dim2, t_dim3, t_dim4 = t.shape[0], t.shape[1], t.shape[2], t.shape[3], t.shape[4]
-                # # print(s.shape)
-                # # print(d.shape)
-                # # print(t.shape)
-                s = s.reshape(dim0*dim1,dim2)
-                s = s.repeat(t_dim2,1)
-                # print(s.shape)
-                d = d.reshape(dim0*dim1,dim2)
-                d = d.repeat(t_dim2,1)
-                # print(d.shape)
+      
+                s = s.repeat(1,1,t_dim2)
+
+                s = s.reshape(dim0*dim1*t_dim2,dim2)
+     
+                #s = s.repeat(t_dim2,1)
+              
+                d = d.repeat(1,1,t_dim2)
+                d = d.reshape(dim0*dim1*t_dim2,dim2)
+                #d = d.repeat(t_dim2,1)
+    
                 t2 = t.reshape(t_dim0*t_dim1,t_dim2,t_dim3,t_dim4)
-                # print('t')
-                # print(t2.shape)
+     
                 t = t.reshape(t_dim0*t_dim1*t_dim2,t_dim3,t_dim4)
-                # print(t.shape)
+                self.t = t
+         
+                torch.save(t,'tritest.pt')
+                torch.save(s, 'sourcetest.pt')
                 distances,pt = self._ray_distance(s,d,t)
                 
                 distances = distances.reshape(dim0,dim1,t_dim2)
+     
                 pt = pt.reshape(dim0,dim1,t_dim2,3)
                 # Find the maximum value of the 100 triangles to extract the "heightmap" value.
-                # #print(torch.max(distances,2).values.shape)
+                
                 indices =torch.max(distances,2).indices
                 distances = torch.max(distances,2).values
                 indices = indices.unsqueeze(dim=2).unsqueeze(dim=2).repeat(1,1,1,3)
@@ -82,10 +98,10 @@ class Camera():
                 pt = pt.squeeze(dim=2)
                # pt = pt[:,:,indices]
                 #distances = distances.reshape(dim0,dim1)
-                #print(distances.shape)
+
                 if output_distances is None:
-                    output_distances = torch.tensor(distances,device=self.device,)
-                    output_pt = torch.tensor(pt,device=self.device,)
+                    output_distances = distances.clone().detach()#torch.tensor(distances,device=self.device,)
+                    output_pt = pt.clone().detach()#torch.tensor(pt,device=self.device,)
                 else: 
                     output_distances = torch.cat((output_distances,distances),1)
                     output_pt = torch.cat((output_pt,pt),1)
@@ -96,21 +112,29 @@ class Camera():
             d = directions.reshape(dim0*dim1,dim2)
             t = triangles.reshape(t_dim0*t_dim1*t_dim2,t_dim3,t_dim4)
             output_distances = self._ray_distance(s,d,t)
-        #print(output_distances.shape)
-        # print(output_distances)
-        # print(indices)
-        # print(output_pt[:,indices].shape)
-        # print(output_pt.shape)
+
         
-         
-        draw_depth(sources.reshape(40,3),output_pt.reshape(40,3))
-        return output_distances
+        d1,d2,d3 = sources.shape[0],sources.shape[1] ,sources.shape[2] 
+        try:
+            draw_depth(sources.reshape(d1*d2,d3),output_pt.reshape(d1*d2,d3))
+        except:
+            print("Isaac Sim not running")
+        return output_distances, output_pt, sources, self.m, self.n, self.k, self.k_after_check, t,self.a,self.b,self.c,self.d,self.g,self.triangles11111,self.sourcesLong
 
     def _load_triangles(self):
         map_values = torch.load("tasks/utils/map_values.pt")
         return map_values
 
+    def _load_triangles_with_indices(self):
+        map_indices = torch.load("tasks/utils/map_indices.pt")
+        map_indices  = map_indices.swapaxes(0,1)
+        map_indices  = map_indices.swapaxes(1,2)
+        triangles = torch.load("tasks/utils/triangles.pt")
+        vertices = torch.load("tasks/utils/vertices.pt")
+        return map_indices, triangles, vertices
+
     def _ray_distance(self, sources: torch.Tensor, directions: torch.Tensor, triangles: torch.Tensor):
+     
         # This is for multiple rays intersection with ONE triangle each
         # Sources: [n_rays, 3]
         # Direction: [n_rays, 3]
@@ -118,89 +142,78 @@ class Camera():
 
         # Sources is 
         # Normalize and opposite of direction
-        #print(directions)
+     
         #d = -(directions/directions.abs().sum())#(directions.swapaxes(0,1)[:] * 1/torch.sum(sources,dim=1)).swapaxes(0,1)
-        d = -torch.nn.functional.normalize(directions)
-        #print(d)
-        # print(sources.shape)
-        # a = torch.ones([k_len,3], device='cuda:0') 
-        # b = torch.ones([k_len,3], device='cuda:0')*2 - a
-        # c = torch.ones([k_len,3], device='cuda:0')*3 - a
-        # print(triangles.shape)
-        # a = triangles[:,0]
-        # b = triangles[:,1] - a
-        # c = triangles[:,2] - a
-        a = triangles[:,2]
-        b = triangles[:,1] - a
-        c = triangles[:,0] - a
-        #print(c)
-        #print(triangles[0])
-        #print(sources[0])
-
-        g = sources - a 
-
-        # print(b.shape)
-        # print(c.shape)
-        # print(d.shape)
-        # print(g.shape)
-        # print("A")
-        # print(a)
-        # print(b)
-        # print(c)
-        # print(d)
-        # print(g)
-        # print("stop")
-        #torch.cuda.synchronize()
-        a=a.swapaxes(0,1)
-        b=b.swapaxes(0,1)
-        c=c.swapaxes(0,1)
-        d=d.swapaxes(0,1)
-        g=g.swapaxes(0,1)
+     
         
-        #for i in range(10):
-        # print(d)
-        # print(c)
-        kk = b.float().cross(c.float())
-        # print(kk.shape)
-        #kk = kk* b
-        det2=torch.mm(torch.transpose(kk,0,1),d.float())
-        # print(det2)
+        d = -torch.nn.functional.normalize(directions) # 4000 x 3 # not checked
+        
+ 
+        a = triangles[:,2]      # 4000 x 3 # checked 
+        b = triangles[:,1] - a  # 4000 x 3 # checked 
+        c = triangles[:,0] - a  # 4000 x 3 # checked
+
+        g = sources - a     # 4000 x 3 # not checked but i dont think this is it
+
+        self.a = a
+        self.g = g
+        self.sourcesLong = sources
+        a=a.swapaxes(0,1)   # 3 x 4000   
+        b=b.swapaxes(0,1)   # 3 x 4000
+        c=c.swapaxes(0,1)   # 3 x 4000
+        d=d.swapaxes(0,1)   # 3 x 4000
+        g=g.swapaxes(0,1)   # 3 x 4000
+        
+
+    
+       # kk = b.float().cross(c.float()) 
+
+
+       ## det2=torch.mm(torch.transpose(kk,0,1),d.float())
+        detdet = b.half().cross(c.half()) 
+        
+        # 4000 
         det = (b[0][:] * c[1][:]* d[2][:] + c[0][:]*d[1][:]*b[2][:]+d[0][:]*b[1][:]*c[2][:]) - (b[2][:] * c[1][:]* d[0][:] + c[2][:]*d[1][:]*b[0][:]+d[2][:]*b[1][:]*c[0][:])
-        # print(det)
-        #det = torch.linalg.det(detdet)
-        # print(det)
-        #del a
+
+        det = (detdet[0][:]*d[0][:]+detdet[1][:]*d[1][:]+detdet[2][:]*d[2][:])
+
+       
 
         nn = g.half().cross(c.half())
         nn = (nn[0][:]*d[0][:]+nn[1][:]*d[1][:]+nn[2][:]*d[2][:])/det
+        # 4000
         #n = torch.where(det == 0,-99.0,(g[0][:] * c[1][:]* d[2][:] + c[0][:]*d[1][:]*g[2][:]+d[0][:]*g[1][:]*c[2][:]) - (g[2][:] * c[1][:]* d[0][:] + c[2][:]*d[1][:]*g[0][:]+d[2][:]*g[1][:]*c[0][:]) / det)
-        n = torch.where(det == 0,-99.0,nn)
+        n = torch.where(det == 0.0,-99.0,nn.double())
         mm = b.half().cross(g.half())
         mm = (mm[0][:]*d[0][:]+mm[1][:]*d[1][:]+mm[2][:]*d[2][:])/det
+        # 4000
+
         #m = torch.where(det == 0,-99.0,(b[0][:] * g[1][:]* d[2][:] + g[0][:]*d[1][:]*b[2][:]+d[0][:]*b[1][:]*g[2][:]) - (b[2][:] * g[1][:]* d[0][:] + g[2][:]*d[1][:]*b[0][:]+d[2][:]*b[1][:]*g[0][:]) / det) 
-        m = torch.where(det == 0,-99.0,mm) 
-        
-        # g,c,d
-        #n = g[0][:]*b[1][:]*d[2][:]
-        #del d
-        k = torch.where(det == 0,-99.0,(b[0][:] * c[1][:]* g[2][:] + c[0][:]*g[1][:]*b[2][:]+g[0][:]*b[1][:]*c[2][:]) - (b[2][:] * c[1][:]* g[0][:] + c[2][:]*g[1][:]*b[0][:]+g[2][:]*b[1][:]*c[0][:]) / det)  
-        # print(det)
-        # print(n)
-        # print(m)
-        # print(k)
+        m = torch.where(det == 0.0,-99.0,mm.double()) 
+        kk = b.half().cross(c.half())
+        kk = (kk[0][:]*g[0][:]+kk[1][:]*g[1][:]+kk[2][:]*g[2][:])/det
+        #k = torch.where(det == 0.0,-99.0,(b[0][:] * c[1][:]* g[2][:] + c[0][:]*g[1][:]*b[2][:]+g[0][:]*b[1][:]*c[2][:]) - (b[2][:] * c[1][:]* g[0][:] + c[2][:]*g[1][:]*b[0][:]+g[2][:]*b[1][:]*c[0][:]) / det)  
+        k = torch.where(det == 0.0,-99.0,kk.double()) 
         #TODO implement append functionality
         #k_after_check = torch.where(((n >= 0.0) & (m >= 0.0) & (n + m <= 1.0) & (k >= 0.0)),k,-99.0) 
-        # print("n")
-        # print(n)
-        # print(m)
+
         k_after_check = torch.where(((n >= 0.0) & (m >= 0.0) & (n + m <= 1.0)),k,-99.0)
-        # print(sources.shape)
-        # # print(directions)
-        # print(((-d*k_after_check)[:,0]).shape)
-        # print(((-d*k_after_check)).shape)
+    
+
         pt = sources -(d*k_after_check).swapaxes(0,1)
+
        # pt = torch.where(k_after_check)
         # print(k.shape)
+        #TODO remove
+        self.m = m
+        self.n = n
+        self.k = k
+        self.k_after_check = k_after_check
+        self.b = b
+        self.c = c
+        self.d = d
+        self.triangles11111 = triangles
+
         return k_after_check, pt
 
     def _depth_transform(self, rover_l, rover_r, rover_depth_points):
@@ -247,15 +260,31 @@ class Camera():
         rover_dir = torch.cat((x, y, z),1).unsqueeze(1)
         # print("roverdir")
         
-
+        
         rover_dir = rover_dir.repeat(1,num_points-1,1)#.swapaxes(0,1)
         #Stack points in a [x, y, 3] matrix, and return
         sources = torch.stack((x_p[:,0:num_points-1], y_p[:,0:num_points-1], z_p[:,0:num_points-1]), 2)
         # print(sources.shape)
         # print(rover_dir.shape)
+        #print(rover_dir)
         return sources, rover_dir
 
-    def _heightmap_distribution(self, x_limit=0.2, y_limit=0.2, square=False, y_start=0.0, delta=0.1, front_heavy=0, plot=False):
+    def _plot_mesh(self, triangles_with_indices):
+        tri = self.triangles[triangles_with_indices.long()]
+        vert = self.vertices
+        mesh = o3d.geometry.TriangleMesh()
+        np_triangles = tri.cpu().detach().numpy()
+        np_vertices = vert.cpu().detach().numpy()
+        mesh.vertices = o3d.utility.Vector3dVector(np_vertices)
+        mesh.triangles = o3d.utility.Vector3iVector(np_triangles)
+        o3d.visualization.draw_geometries([mesh])
+    
+    def plot_first(self):
+        triangles_with_indices = self.map_indices[200, 100]
+        triangles_with_indices = triangles_with_indices.flatten()
+        self._plot_mesh(triangles_with_indices)
+
+    def _heightmap_distribution(self, x_limit=1.12, y_limit=1.8, square=False, y_start=0.5, delta=0.2, front_heavy=0, plot=False):
 
         point_distribution = []
 
@@ -294,7 +323,8 @@ class Camera():
                     x -= delta
                     xd -= 1
                 else:
-                    point_distribution.append([x, -y, 0])
+                    #point_distribution.append([x, -y, -0.26878])
+                    point_distribution.append([y, -x, -0.26878])
                     x += delta
                     xd += 1
 
@@ -348,9 +378,11 @@ class Camera():
         # Get nearets array of triangles for searching
         triangles = triangle_matrix[x, y]
         triangles = triangles.reshape([depth_points.shape[0],depth_points.shape[1],triangle_matrix.shape[2],triangle_matrix.shape[3],triangle_matrix.shape[4]])
+        triangles_with_indices = self.map_indices[x, y]
+
 
         # Return the found heights
-        return triangles
+        return triangles, triangles_with_indices
     
     def _heightmap_overlay(self,dim, point_distrubution):
         zeros = torch.zeros_like(point_distrubution[:,0])
@@ -361,14 +393,90 @@ class Camera():
 
         return overlay
 
-# a = torch.tensor([0.0,0.0],device='cuda:0')
-# cam = Camera('cuda:0', a)
+a = torch.tensor([0.0,0.0],device='cuda:0')
+cam = Camera('cuda:0', a)
 
-# source = torch.tensor([[[-0.1,-0.1,1.0]]],device='cuda:0',dtype=torch.float64)
-# direction = torch.tensor([[[-0.5,-0.5,1]]],device='cuda:0',dtype=torch.float64)
+source = torch.tensor([[[-0.1,-0.1,1.0]]],device='cuda:0',dtype=torch.float16)
+direction = torch.tensor([[[-0.5,-0.5,1]]],device='cuda:0',dtype=torch.float16)
+#print(cam.get_depths_test(source,direction))
 # #print(source.shape)
 # #print(direction.shape)
+pos2 = torch.tensor([
+        [6.5921, 5.5467, 0.6557],
+        [5.4156, 4.4939, 0.7105],
+        [5.4735, 5.5226, 0.6976],
+        [4.5162, 4.5003, 0.6982],
+        [4.5202, 5.5012, 0.7037],
+        [3.5420, 4.5217, 0.7294]], device='cuda:0')
+rotations2 = torch.tensor([
+        [-0.0949,  0.1376, -0.0917],
+        [ 0.1036, -0.0723, -0.0339],
+        [-0.1516, -0.0358, -0.0008],
+        [ 0.0136,  0.0715, -0.0126],
+        [-0.0114,  0.0963, -0.0064],
+        [-0.0706, -0.0316,  0.0137]], device='cuda:0')
+rotationsz = torch.tensor([
+        [0.0,  0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0]], device='cuda:0')
+pos = torch.tensor([
+        [3.5420, 4.5217, 1.7294],
+        [3.5420, 4.5217, 0.7294]], device='cuda:0')
+rotations = torch.tensor([
+        [-0.0,  0.0, -0.0],
+        [-0.0,  0.0, -0.0]], device='cuda:0')
+pos4 = torch.tensor([
+        [3.5420, 4.5217, 1.7294],
+        [3.5420, 4.5217, 0.7294],
+        [2.5420, 4.5217, 0.7294],
+        [3.5420, 4.5217, 0.7294]], device='cuda:0')
+rotations4 = torch.tensor([
+        [0.0,  0.0, 0.0],
+        [ 0.0,  0.0, 0.0],
+        [ 0.0,  0.0, 0.0],
+        [ 0.0,  0.0, 0.0]], device='cuda:0')
+pos3 = torch.tensor([
+        [3.5420, 4.5217, 0.7294]], device='cuda:0')
+rotations3 = torch.tensor([
+        [-0.0,  0.0, -0.0]], device='cuda:0')
+pos5 = torch.tensor([
+        [3.5421-0.2, 4.5217, 0.7294],
+], device='cuda:0')
+rotations5 = torch.tensor([
+        [0.0,  0.0, 0.0],], device='cuda:0')        
 
-# b,pt = cam.get_depths_test(source,direction)
-# #print(b)
-# print(b,pt)
+#output_distances, output_pt, sources, m,n,k,k_after_check, t,a,b,c,d,g,triangleslong,sourcesLong = cam.get_depths(pos2,rotations2)
+# pos5 = torch.tensor([
+#         [3.5421, 4.5217, 0.7294]], device='cuda:0')
+
+# output_distances1, output_pt1, sources1, m1,n1,k1,k_after_check1, t1,a1,b1,c1,d1,g1,triangleslong1,sourcesLong1 = cam.get_depths(pos5,rotations5)
+# print(m1.shape)
+
+
+# print(a.shape)
+# print(a1.shape)
+# print(triangleslong.shape)
+# print("triangles long: ", torch.sum(triangleslong[100:200]-triangleslong1[0:100]))
+# print("Sources: ", torch.sum(sourcesLong[100:200]-sourcesLong1[0:100]))
+# print("D: ", torch.sum(d[:,100:200]-d1[:,0:100]))
+# print("G: ", torch.sum(g[100:200]-g1[0:100]))
+# print("M: ", torch.sum(m[100:200]-m1[0:100]))
+# print("N: ", torch.sum(n[100:200]-n1[0:100]))
+# print("K: ", torch.sum(k[100:200]-k1[0:100]))
+
+# #print(sourcesLong.shape)
+# #print(sourcesLong[0:100])
+
+
+#print(output_distances)
+# print(output_pt[0])
+# print(sources[0])
+# print(output_distances1[0])
+# # print(output_pt1[0])
+# # print(sources1[0])
+# # # # # #cam.plot_first()
+
+
