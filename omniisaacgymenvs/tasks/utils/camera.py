06edf8b38924cc68e6548ece5c2c57acd03230a3
abmoRobotics <abmoRobotics@student.aau.dk>
2022-11-01 18:29:04 +0100

    Camera improvement, reward functions, observation

--- omniisaacgymenvs/tasks/utils/camera.py
+++ omniisaacgymenvs/tasks/utils/camera.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 import torch
 import open3d as o3d
 from omniisaacgymenvs.tasks.utils.heightmap_distribution import heightmap_distribution
-from omniisaacgymenvs.tasks.utils.rover_depth import draw_depth
+from omniisaacgymenvs.tasks.utils.debug_utils import draw_depth
+from omniisaacgymenvs.tasks.utils.ray_casting import ray_distance
 import time
+
 class Camera():
     """Ray–Triangle intersection based on Möller–Trumbore intersection algorithm
     
@@ -36,7 +38,7 @@ class Camera():
         debug : bool, optional
             Enables debugging functionalities (default is False)
         """
-
+        self.debug = debug
         self.device = device    
         self.partition = True    
         self.num_partitions = 1 # Slices the input data to reduce VRAM usage. 
@@ -44,31 +46,12 @@ class Camera():
         #self.map_values = self._load_triangles()    # Load triangles into an [1200,1200, 100, 3, 3] array
         self.map_indices, self.triangles, self.vertices = self._load_triangles_with_indices()   # Load into [1200,1200, 100, 3] array
         self.heightmap_distribution = self._heightmap_distribution()    # Get distribution of points in the local reference frame of the rover
+        self.num_exteroceptive = self.heightmap_distribution.shape[0] 
         self.shift = shift          # Shift of the map 
         self.dtype = torch.float16  # data type for performing the calculations
 
-#TODO Remove this function
-    def get_depths_test(self, sources, directions):
-        # Test function
-        triangles = self._height_lookup(self.map_values,sources[:,:,0:2],self.horizontal,self.shift)
-        triangles = torch.tensor([[[[[0.0,0.0,0.0],[0.0,2.0,0.0],[2.0,0.0,0.0]],[[0.0,0.0,0.2],[0.0,2.0,0.2],[2.0,0.0,0.2]],[[0.0,0.0,0.3],[0.0,2.0,0.3],[2.0,0.0,0.3]]]]],device='cuda:0')
-        dim0, dim1, dim2 = sources.shape[0], sources.shape[1], sources.shape[2]
-        t_dim0, t_dim1, t_dim2, t_dim3, t_dim4 = triangles.shape[0], triangles.shape[1], triangles.shape[2], triangles.shape[3], triangles.shape[4] 
-        s = sources.reshape(dim0*dim1,dim2)
-        d = directions.reshape(dim0*dim1,dim2)
-        t = triangles.reshape(t_dim0*t_dim1*t_dim2,t_dim3,t_dim4)
-
-        distances,pt = self._ray_distance(s,d,t)
-        distances = distances.reshape(dim0,dim1,t_dim2)
-        pt = pt.reshape(dim0,dim1,t_dim2,3)
-        # Find the maximum value of the 100 triangles to extract the "heightmap" value.
-        indices =torch.max(distances,2).indices
-        distances = torch.max(distances,2).values
-        indices = indices.unsqueeze(dim=2).unsqueeze(dim=2).repeat(1,1,1,3)
-        pt = pt.gather(dim=2,index=indices)
-        pt = pt.squeeze(dim=2)
-        return distances,pt
-
+    def get_num_exteroceptive(self):
+        return self.num_exteroceptive
 
     def get_depths(self, positions, rotations):
         """
@@ -121,7 +104,7 @@ class Camera():
                 #torch.save(s, 'sourcetest.pt')
                 self.initialmemory2 = torch.cuda.memory_reserved(0)
                 
-                distances,pt = self._ray_distance(s,d,t)
+                distances,pt = ray_distance(s,d,t)
                 self.initialmemory3 = torch.cuda.memory_reserved(0)
                 distances = distances.reshape(dim0,dim1,t_dim2)
      
@@ -148,122 +131,35 @@ class Camera():
             s = sources.reshape(dim0*dim1,dim2)
             d = directions.reshape(dim0*dim1,dim2)
             t = triangles.reshape(t_dim0*t_dim1*t_dim2,t_dim3,t_dim4)
-            output_distances = self._ray_distance(s,d,t)
+            output_distances = ray_distance(s,d,t)
 
         
         d1,d2,d3 = sources.shape[0],sources.shape[1] ,sources.shape[2] 
         #print((self.initialmemory3 - torch.cuda.memory_reserved(0))/1_000_000_000)
-        try:
-            pass
-            #draw_depth(sources.reshape(d1*d2,d3),output_pt.reshape(d1*d2,d3))
-        except:
-            print("Isaac Sim not running")
+        if self.debug:
+            try:
+                draw_depth(sources.reshape(d1*d2,d3),output_pt.reshape(d1*d2,d3))
+            except:
+                print("Isaac Sim not running")
         return output_distances, output_pt, sources
 
     def _load_triangles(self):
         """Loads triangles with explicit values, each triangle is a 3 x 3 matrix"""
-        map_values = torch.load("tasks/utils/map_values.pt")
+        map_values = torch.load("tasks/utils/terrain/knn_terrain/map_values.pt")
         map_values=map_values.swapaxes(0,1)
         map_values=map_values.swapaxes(1,2)
         return map_values
 
     def _load_triangles_with_indices(self):
         """Loads triangles with indicies to vertices, each triangle is a 3D vector"""
-        map_indices = torch.load("tasks/utils/map_indices.pt")
+        map_indices = torch.load("tasks/utils/terrain/knn_terrain/map_indices.pt")
         map_indices  = map_indices.swapaxes(0,1)
         map_indices  = map_indices.swapaxes(1,2)
-        triangles = torch.load("tasks/utils/triangles.pt")
-        vertices = torch.load("tasks/utils/vertices.pt")
+        triangles = torch.load("tasks/utils/terrain/knn_terrain/triangles.pt")
+        vertices = torch.load("tasks/utils/terrain/knn_terrain/vertices.pt")
         return map_indices, triangles, vertices
 
-    def _ray_distance(self, sources: torch.Tensor, directions: torch.Tensor, triangles: torch.Tensor):
-        """
-        Checks if there is an intersection between a triangle and calculates the distance based on Möller–Trumbore intersection algorithm
 
-        Parameters
-        ----------
-        sources : torch.tensor
-            3D points of the ray source – dimension: [n_rays, 3]
-        directions : torch.tensor
-            Direction vector for the ray – dimension: [n_rays, 3]
-        triangles : torch.tensor
-            Triangles for ray intersection – dimension: [n_rays, 3, 3]
-        """
-        # This is for multiple rays intersection with ONE triangle each
-        # Sources: [n_rays, 3]
-        # Direction: [n_rays, 3]
-        # Direction: [n_rays, 3, 3]
-
-        # Sources is 
-        # Normalize and opposite of direction
-     
-        #d = -(directions/directions.abs().sum())#(directions.swapaxes(0,1)[:] * 1/torch.sum(sources,dim=1)).swapaxes(0,1)
-     
-        # Normalize and opposite of direction
-        d = -torch.nn.functional.normalize(directions) # 4000 x 3 
-        
- 
-        a = triangles[:,2]      # 4000 x 3 
-        b = triangles[:,1] - a  # 4000 x 3 
-        c = triangles[:,0] - a  # 4000 x 3 
-
-        g = sources - a     # 4000 x 3 
-
-        self.a = a
-        self.g = g
-        self.sourcesLong = sources
-        a=a.swapaxes(0,1)   # 3 x 4000   
-        b=b.swapaxes(0,1)   # 3 x 4000
-        c=c.swapaxes(0,1)   # 3 x 4000
-        d=d.swapaxes(0,1)   # 3 x 4000
-        g=g.swapaxes(0,1)   # 3 x 4000
-
-        zeros = torch.zeros(g.shape[1],device=self.device,dtype=self.dtype)
-        ones = torch.ones(g.shape[1],device=self.device,dtype=self.dtype)
-        error_tensor = ones * -99.0
-
-       # kk = b.float().cross(c.float()) 
-       
-       ## det2=torch.mm(torch.transpose(kk,0,1),d.float())
-        detdet = b.cross(c) 
-        
-        # 4000 
-        #det = (b[0][:] * c[1][:]* d[2][:] + c[0][:]*d[1][:]*b[2][:]+d[0][:]*b[1][:]*c[2][:]) - (b[2][:] * c[1][:]* d[0][:] + c[2][:]*d[1][:]*b[0][:]+d[2][:]*b[1][:]*c[0][:])
-
-        det = (detdet[0][:]*d[0][:]+detdet[1][:]*d[1][:]+detdet[2][:]*d[2][:])
-        del detdet
-        
-        # Calculate Scalar Triple Product for [g,c,d] 
-        nn = g.cross(c)
-        nn = (nn[0][:]*d[0][:]+nn[1][:]*d[1][:]+nn[2][:]*d[2][:])/det
-        n = torch.where(det == zeros,error_tensor,nn)
-        del nn
-
-        # Calculate Scalar Triple Product for [b,g,d]
-        mm = b.cross(g)
-        mm = (mm[0][:]*d[0][:]+mm[1][:]*d[1][:]+mm[2][:]*d[2][:])/det
-        m = torch.where(det == ones,error_tensor,mm) 
-        del mm
-
-        # Calculate Scalar Triple Product for [b,c,g]
-        kk = b.cross(c)
-        kk = (kk[0][:]*g[0][:]+kk[1][:]*g[1][:]+kk[2][:]*g[2][:])/det
-        #k = torch.where(det == 0.0,-99.0,(b[0][:] * c[1][:]* g[2][:] + c[0][:]*g[1][:]*b[2][:]+g[0][:]*b[1][:]*c[2][:]) - (b[2][:] * c[1][:]* g[0][:] + c[2][:]*g[1][:]*b[0][:]+g[2][:]*b[1][:]*c[0][:]) / det)  
-        k = torch.where(det == ones,error_tensor,kk) 
-        del kk
-
-        # filter out based on the condition ((n >= 0.0) & (m >= 0.0) & (n + m <= 1.0))
-        k_after_check = torch.where(((n >= zeros) & (m >= zeros) & (n + m <= ones)),k,error_tensor)
-
-        # Calucate the intersection point
-        pt = sources -(d*k_after_check).swapaxes(0,1)
-        
-        #torch.cuda.empty_cache()
-
-
-        #print((self.initialmemory2 - torch.cuda.memory_reserved(0))/1_000_000_000)
-        #time.sleep(20)
-        return k_after_check, pt
 
     def _depth_transform(self, rover_l, rover_r, rover_depth_points):
         """Transforms the local heightmap of the rover, into the global frame
@@ -395,8 +291,10 @@ class Camera():
             ax.set_aspect('equal')
             plt.show()
 
-        return torch.tensor(point_distribution, device=self.device)
-
+        #self.num_exteroceptive = 
+        distribution = torch.tensor(point_distribution, device=self.device)
+        print(distribution.shape)
+        return distribution
     def _limit_at_x(self, x):
         return x*(4.3315)-0.129945
 
@@ -454,10 +352,6 @@ class Camera():
 if __name__ == "__main__":
     a = torch.tensor([0.0,0.0],device='cuda:0')
     cam = Camera('cuda:0', a)
-
-    # source = torch.tensor([[[-0.1,-0.1,1.0]]],device='cuda:0',dtype=torch.float16)
-    # direction = torch.tensor([[[-0.5,-0.5,1]]],device='cuda:0',dtype=torch.float16)
-
     pos2 = torch.tensor([
             [6.5921, 5.5467, 0.6557],
             [5.4156, 4.4939, 0.7105],
@@ -472,68 +366,6 @@ if __name__ == "__main__":
             [ 0.0136,  0.0715, -0.0126],
             [-0.0114,  0.0963, -0.0064],
             [-0.0706, -0.0316,  0.0137]], device='cuda:0',dtype=torch.float16)
-    # rotationsz = torch.tensor([
-    #         [0.0,  0.0, 0.0],
-    #         [0.0, 0.0, 0.0],
-    #         [0.0, 0.0, 0.0],
-    #         [0.0, 0.0, 0.0],
-    #         [0.0, 0.0, 0.0],
-    #         [0.0, 0.0, 0.0]], device='cuda:0')
-    # pos = torch.tensor([
-    #         [3.5420, 4.5217, 1.7294],
-    #         [3.5420, 4.5217, 0.7294]], device='cuda:0')
-    # rotations = torch.tensor([
-    #         [-0.0,  0.0, -0.0],
-    #         [-0.0,  0.0, -0.0]], device='cuda:0')
-    # pos4 = torch.tensor([
-    #         [3.5420, 4.5217, 1.7294],
-    #         [3.5420, 4.5217, 0.7294],
-    #         [2.5420, 4.5217, 0.7294],
-    #         [3.5420, 4.5217, 0.7294]], device='cuda:0')
-    # rotations4 = torch.tensor([
-    #         [0.0,  0.0, 0.0],
-    #         [ 0.0,  0.0, 0.0],
-    #         [ 0.0,  0.0, 0.0],
-    #         [ 0.0,  0.0, 0.0]], device='cuda:0')
-    # pos3 = torch.tensor([
-    #         [3.5420, 4.5217, 0.7294]], device='cuda:0')
-    # rotations3 = torch.tensor([
-    #         [-0.0,  0.0, -0.0]], device='cuda:0')
-    # pos5 = torch.tensor([
-    #         [3.5421-0.2, 4.5217, 0.7294],
-    # ], device='cuda:0')
-    # rotations5 = torch.tensor([
-    #        [0.0,  0.0, 0.0],], device='cuda:0')        
-    #print(pos2.repeat(10,1).shape)
 
     output_distances, output_pt, sources = cam.get_depths(pos2.repeat(100,1),rotations2.repeat(100,1))
-    # pos5 = torch.tensor([
-    #         [3.5421, 4.5217, 0.7294]], device='cuda:0')
-
-    # output_distances1, output_pt1, sources1, m1,n1,k1,k_after_check1, t1,a1,b1,c1,d1,g1,triangleslong1,sourcesLong1 = cam.get_depths(pos5,rotations5)
-    # print(m1.shape)
-
-    # print(a.shape)
-    # print(a1.shape)
-    # print(triangleslong.shape)
-    # print("triangles long: ", torch.sum(triangleslong[100:200]-triangleslong1[0:100]))
-    # print("Sources: ", torch.sum(sourcesLong[100:200]-sourcesLong1[0:100]))
-    # print("D: ", torch.sum(d[:,100:200]-d1[:,0:100]))
-    # print("G: ", torch.sum(g[100:200]-g1[0:100]))
-    # print("M: ", torch.sum(m[100:200]-m1[0:100]))
-    # print("N: ", torch.sum(n[100:200]-n1[0:100]))
-    # print("K: ", torch.sum(k[100:200]-k1[0:100]))
-
-    # #print(sourcesLong.shape)
-    # #print(sourcesLong[0:100])
-
-
-    #print(output_distances)
-    # print(output_pt[0])
-    # print(sources[0])
-    # print(output_distances1[0])
-    # # print(output_pt1[0])
-    # # print(sources1[0])
-    # # # # # #cam.plot_first()
-
-
+    print(output_distances.shape)
---
+++ omniisaacgymenvs/train.py
@@ -0,0 +1,200 @@
+import torch
+import torch.nn as nn
+
+# Import the skrl components to build the RL system
+from skrl.models.torch import Model, GaussianMixin, DeterministicMixin
+from skrl.memories.torch import RandomMemory
+from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
+from skrl.resources.schedulers.torch import KLAdaptiveRL
+from skrl.resources.preprocessors.torch import RunningStandardScaler
+from skrl.trainers.torch import SequentialTrainer
+from skrl.envs.torch import wrap_env
+from skrl.envs.torch import load_omniverse_isaacgym_env
+from skrl.utils import set_seed
+from learning.model import StochasticActorHeightmap, DeterministicHeightmap
+import hydra
+from omegaconf import DictConfig
+from hydra import compose, initialize
+import wandb
+import datetime
+#cfg_ppo = PPO_DEFAULT_CONFIG.copy()
+
+# set the seed for reproducibility
+set_seed(42)
+
+#@hydra.main(config_name="config", config_path="cfg")
+def parse_hydra_configs():
+    initialize(config_path="cfg", job_name="test_app")
+    cfg = compose(config_name="config")
+
+    cfg_ppo = PPO_DEFAULT_CONFIG.copy()
+    cfg_network = cfg.trainSKRL.network
+    cfg_experiment = cfg.trainSKRL.experiment
+    # Set all parameters according to cfg file
+    for param, value in (cfg.trainSKRL.config).items():
+        cfg_ppo[param] = value
+    hydra.core.global_hydra.GlobalHydra.instance().clear()
+    run_simulation(cfg_ppo, cfg_network, cfg_experiment)
+
+
+
+def run_simulation(cfg_ppo, cfg_network, cfg_experiment):
+
+    #print(cfg_ppo)
+    cfg_ppo = PPO_DEFAULT_CONFIG.copy()
+    print(cfg_network.mlp.layers)
+    exit()
+    # Load and wrap the Omniverse Isaac Gym environment
+    env = load_omniverse_isaacgym_env(task_name="Rover")
+    env = wrap_env(env)
+    
+    device = env.device
+
+    # Instantiate a RandomMemory as rollout buffer (any memory can be used for this)
+    memory = RandomMemory(memory_size=60, num_envs=env.num_envs, device=device)
+
+    # Get values from cfg
+    mlp_layers = cfg_network.mlp.layers
+    encoder_layers = cfg_network.encoder.layers
+    activation_function = cfg_network.mlp.activation
+
+    # Instantiate the agent's models (function approximators).
+    models_ppo = {  "policy": StochasticActorHeightmap(env.observation_space, env.action_space, network_features=mlp_layers, encoder_features=encoder_layers, activation_function=activation_function),
+                "value": DeterministicHeightmap(env.observation_space, env.action_space, network_features=mlp_layers, encoder_features=encoder_layers ,activation_function=activation_function)}
+
+    # Instantiate parameters of the model
+    for model in models_ppo.values():
+        model.init_parameters(method_name="normal_", mean=0.0, std=0.05)
+
+    # Define agent
+    agent = PPO(models=models_ppo,
+            memory=memory,
+            cfg=cfg_ppo,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            device=device)
+    
+
+    # Configure and instantiate the RL trainer
+    cfg_trainer = {"timesteps": 1600, "headless": False}
+    trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
+
+    # start training
+    trainer.train()
+
+class TrainerSKRL():
+    def __init__(self):
+        self._load_cfg()
+        self.wandb_group = "test-group"
+        time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.wandb_name = f"test-run_{time_str}"
+       # self.start_simulation()
+        #self.start_training()
+
+
+
+    def _load_cfg(self):
+        initialize(config_path="cfg", job_name="test_app")
+        cfg = compose(config_name="config")
+
+        self.cfg_ppo = PPO_DEFAULT_CONFIG.copy()
+        self.cfg_network = cfg.trainSKRL.network
+        self.cfg_experiment = cfg.trainSKRL.experiment
+        # Set all parameters according to cfg file
+        for param, value in (cfg.trainSKRL.config).items():
+            self.cfg_ppo[param] = value
+        
+        hydra.core.global_hydra.GlobalHydra.instance().clear()
+
+    def start_simulation(self):
+        env = load_omniverse_isaacgym_env(task_name="Rover")
+        self.env = wrap_env(env)
+
+    def train(self):
+        env = self.env
+        device = env.device
+
+        
+        # Instantiate a RandomMemory as rollout buffer (any memory can be used for this)
+        memory = RandomMemory(memory_size=60, num_envs=self.env.num_envs, device=device)
+
+        # Get values from cfg
+        mlp_layers = self.cfg_network.mlp.layers
+        encoder_layers = self.cfg_network.encoder.layers
+        activation_function = self.cfg_network.mlp.activation
+
+        # Instantiate the agent's models (function approximators).
+        models_ppo = {  "policy": StochasticActorHeightmap(env.observation_space, env.action_space, network_features=mlp_layers, encoder_features=encoder_layers, activation_function=activation_function),
+                    "value": DeterministicHeightmap(env.observation_space, env.action_space, network_features=mlp_layers, encoder_features=encoder_layers ,activation_function=activation_function)}
+
+        # Instantiate parameters of the model
+        for model in models_ppo.values():
+            model.init_parameters(method_name="normal_", mean=0.0, std=0.05)
+
+        # Define agent
+        agent = PPO(models=models_ppo,
+                memory=memory,
+                cfg=self.cfg_ppo,
+                observation_space=env.observation_space,
+                action_space=env.action_space,
+                device=device)
+        
+
+        # Configure and instantiate the RL trainer
+        cfg_trainer = {"timesteps": 4000, "headless": True}
+        trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
+
+        # start training
+        trainer.train()
+
+    def start_training_sweep(self,n_sweeps):
+        self.start_simulation()
+       # Define sweep config
+        sweep_configuration = {
+            'method': 'bayes',
+            'name': 'sweep',
+            'metric': {'goal': 'maximize', 'name': 'Reward / Total reward (mean)'},
+            'parameters': 
+            {
+                'mini_batches': {'values': [4, 8]},
+                'lr': {'max': 0.003, 'min': 0.00003}
+                
+            }
+        }
+
+        # Initialize sweep by passing in config. (Optional) Provide a name of the project.
+        sweep_id = wandb.sweep(sweep=sweep_configuration, project='isaac-rover-2.0')
+        wandb.agent(sweep_id, function=self.sweep, count=n_sweeps)
+        # Start sweep job.
+
+
+    def sweep(self):
+        time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.wandb_name = f"test-run_{time_str}"
+        run = wandb.init(project='isaac-rover-2.0', sync_tensorboard=True,name=self.wandb_name,group=self.wandb_group, entity="aalborg-university")
+        self.cfg_ppo["learning_rate"] = wandb.config.lr
+        self.cfg_ppo["mini_batches"] = wandb.config.mini_batches
+        self.train()
+       # wandb.finish()
+
+    def start_training(self):
+        self.start_simulation()
+        wandb.init(project='isaac-rover-2.0', sync_tensorboard=True,name=self.wandb_name,group=self.wandb_group, entity="aalborg-university")
+        self.train()
+        wandb.finish()
+
+
+    def start_training_sequential(self):
+        for i in range(3):
+            print(i)
+            self.train()
+        pass
+if __name__ == '__main__':
+    # Get hyperparameter config
+    trainer = TrainerSKRL()
+   # trainer.start_training_sequential()
+    #trainer.start_training_sweep(4)
+    trainer.start_training()
+    #parse_hydra_configs()
+
+    
